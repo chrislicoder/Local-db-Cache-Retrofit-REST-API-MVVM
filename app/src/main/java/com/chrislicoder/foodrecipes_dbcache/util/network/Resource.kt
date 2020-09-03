1463ef6d995d4310755125156429ff16d411cda6
Chris Li <chrislicoder@gmail.com>
2020-09-03 23:31:38 +1200

    Create the network resource class

---
+++ app/src/main/java/com/chrislicoder/foodrecipes_dbcache/util/network/Resource.kt
@@ -0,0 +1,21 @@
+package com.chrislicoder.foodrecipes_dbcache.util.network
+
+class Resource<T> private constructor(val status: Status, val data: T, val message: String?) {
+    enum class Status {
+        SUCCESS, ERROR, LOADING
+    }
+
+    companion object {
+        fun <T> success(data: T): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String?, data: T): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
